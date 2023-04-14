76f2e16db2d5fc76a29dce58bbe4d0cecdb4e3d5
Aaron Moore <aaron.moore@bbc.co.uk>
2023-04-14 15:29:17 +0100

    update

--- README.md
+++ README.md
@@ -1,21 +1,6 @@
-# Usage
+# Cloudflare Workers in Rust
 
-This template starts you off with a `src/lib.rs` file, acting as an entrypoint for requests hitting your Worker. Feel free to add more code in this file, or create Rust modules anywhere else for this project to use.
-
-With `wrangler`, you can build, test, and deploy your Worker with the following commands:
-
-```sh
-# compiles your project to WebAssembly and will warn of any issues
-$ npm run build
-
-# run your Worker in an ideal development workflow (with a local server, file watcher & more)
-$ npm run dev
-
-# deploy your Worker globally to the Cloudflare network (update your wrangler.toml file for configuration)
-$ npm run deploy
-```
-
-Read the latest `worker` crate documentation here: https://docs.rs/worker
+`todo`
 
 ## WebAssembly
 
--- src/lib.rs
+++ src/lib.rs
@@ -11,7 +11,9 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
 
     router
         .get_async("/", |req, ctx| async move {
-            let coords = req.cf().coordinates().unwrap_or_default();
+            let coords = req.cf().coordinates().unwrap();
+            let postcode = req.cf().postal_code().unwrap();
+            let city = req.cf().city().unwrap();
 
             let region = req.cf().region().unwrap_or_else(|| "unknown region".into());
 
@@ -20,7 +22,7 @@ pub async fn main(req: Request, env: Env, _ctx: worker::Context) -> Result<Respo
             let test_val = kv.get("prevkey").text().await?.unwrap();
 
             return Response::from_json(
-                &json!({ "coords": coords, "region": region, "kv": test_val }),
+                &json!({ "coords": coords, "region": region, "postcode": postcode, "city": city, "kv": test_val}),
             );
         })
         .run(req, env)
